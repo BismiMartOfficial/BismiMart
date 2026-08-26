fe0a27bdc58693b2a7e389010997128c8bf6efdc
BismiMartOfficial <bisnuss0309@gmail.com>
2026-08-26 13:02:19 +0500

    Add service worker with caching functionality
    Implement service worker for caching and offline support.

--- sw.js
+++ sw.js
@@ -1,1 +1,49 @@
+const CACHE_NAME = "bismimart-v1";
 
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request).then(response => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      }).catch(() => {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
